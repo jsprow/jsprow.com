47ff74adeecdea016732359c0b6e2336be04ce7a
jsprow <jsprow@gmail.com>
2016-10-19 20:16:27 -0400

    does ruby work?

---
+++ classroom/quickstart.rb
@@ -0,0 +1,53 @@
+require 'google/apis/classroom_v1'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+
+require 'fileutils'
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'Classroom API Ruby Quickstart'
+CLIENT_SECRETS_PATH = 'client_secret.json'
+CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+                             "classroom-ruby-quickstart.yaml")
+SCOPE = Google::Apis::ClassroomV1::AUTH_CLASSROOM_COURSES_READONLY
+
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or intitiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+  client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(
+    client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(
+      base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the " +
+         "resulting code after authorization"
+    puts url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
+  end
+  credentials
+end
+
+# Initialize the API
+service = Google::Apis::ClassroomV1::ClassroomService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+# List the first 10 courses the user has access to.
+response = service.list_courses(page_size: 10)
+
+puts "Courses:"
+puts "No courses found" if response.courses.empty?
+response.courses.each do |course|
+  puts "- #{course.name} (#{course.id})"
+end
