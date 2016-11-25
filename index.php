2dfbabd8a375e1aa40df929503f38c4ee28d8998
jsprow <jsprow@gmail.com>
2016-11-24 21:12:21 -0500

    update jsprow.com

--- index.php
+++ index.php
@@ -3,22 +3,46 @@
 <head>
   <title>JSprow</title>
 
-  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="main.css">
   <link rel="icon" type="image/png" href="favicon.ico">
 </head>
 <body id="body-duh">
-  <main>
-    <h1>welcome to...</h1>
-    <a href="flexbox/flex.html">...flexbox</a>
-    <a href="ajax/index.php">...ajax</a>
-    <!-- <a href="naughty-words/naughty.php">...databases</a> -->
-    <a href="emailform/emailform.php">...forms that email</a>
-    <a href="wikipedia/index.php">...flashcards scraped from wikipedia</a>
-    <div class="space-invader-box">
-      <div class="space-invader"></div>
-    </div>
-  </main>
+  <div id="video"></div>
+    <main>
+      <h1>welcome to...</h1>
+      <a href="flexbox/flex.html">...flexbox</a>
+      <a href="ajax/index.php">...ajax</a>
+      <a href="emailform/emailform.php">...forms that email</a>
+      <a href="wikipedia/index.php">...flashcards scraped from wikipedia</a>
+      <div class="space-invader-box">
+        <div class="space-invader"></div>
+      </div>
+    </main>
 
   <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="youtubebg.js"></script>
+  <script>
+    $(function() {
+      $('#video').YTPlayer({
+        fitToBackground: true,
+        videoId: 'eORnMWDI_F0',
+        repeat: true,
+        mute: false,
+        events: {
+          'onReady': onPlayerReady
+        }
+      });
+      function onPlayerReady() {
+        var player = $('#video').data('ytPlayer').player;
+        player.pauseVideo();
+      };
+      $('.space-invader-box').click(function() {
+        $('#video').fadeToggle();
+        var player = $('#video').data('ytPlayer').player;
+        player.playVideo();
+      });
+    });
+  </script>
 </body>
 </html>
