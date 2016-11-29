fe1e4df8bb232aa18f2e848b3d61c3828ae6ad7d
jsprow <jsprow@gmail.com>
2016-11-29 16:30:51 -0500

    Update emailform.php

--- emailform/emailform.php
+++ emailform/emailform.php
@@ -7,6 +7,8 @@
 		body {
 			font-size: calc( 14px + (20 - 14) * (100vw - 320px)/(1280 - 320));
   			line-height: calc( 1.3em + (1.5 - 1.3) * ( (100vw - 21em) / (35 - 21)));
+			padding: 0;
+			height: 100vh;
 		}
 		body,
 		label,
@@ -18,9 +20,9 @@
 		form {
 			display: flex;
 			flex-flow: column nowrap;
-			justify-content: space-between;
+			justify-content: space-around;
 			align-items: flex-start;
-			height: 100vh;
+			height: 100%;
 		}
 		label,
 		input,
@@ -35,14 +37,18 @@
 </head>
 <body>
 	<form class="email" action="email.php" method="post">
-		<label for="#name">Name</label>
-		<input id="name" type="text" name="name" required="" placeholder="First Last">
-		<label for="#email">Email</label>
-		<input id="email" type="email" name="email" required="" pattern="(?!(^[.-].*|[^@]*[.-]@|.*\.{2,}.*)|^.{254}.)([a-zA-Z0-9!#$%&'*+\/=?^_`{|}~.-]+@)(?!-.*|.*-\.)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,15}" placeholder="you@example.com">
-		<label for="#phone">Phone Number</label>
-		<input id="phone" type="phone" name="phone" required="" pattern="^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$" placeholder="xxx-xxx-xxxx">
-		<label for="#message">Message</label>
-		<textarea id="message" type="textarea" name="message" required="" placeholder="Please let us know how we can help you."></textarea>
+		<label for="#name">Name
+			<input id="name" type="text" name="name" required="" placeholder="First Last">
+		</label>
+		<label for="#email">Email
+			<input id="email" type="email" name="email" required="" pattern="(?!(^[.-].*|[^@]*[.-]@|.*\.{2,}.*)|^.{254}.)([a-zA-Z0-9!#$%&'*+\/=?^_`{|}~.-]+@)(?!-.*|.*-\.)([a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,15}" placeholder="you@example.com">
+		</label>
+		<label for="#phone">Phone Number
+			<input id="phone" type="phone" name="phone" required="" pattern="^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$" placeholder="xxx-xxx-xxxx">
+		</label>
+		<label for="#message">Message
+			<textarea id="message" type="textarea" name="message" required="" placeholder="Please let us know how we can help you."></textarea>
+		</label>	
 		<div class="g-recaptcha" data-sitekey="6Lf8NwkUAAAAAL10S24CNJEkfOAb-aRKfvaik2W7"></div>
 		<button id="email-submit" type="submit" value="Submit"><i class="fa fa-envelope"></i><span> Submit</span></button>
 	</form>
